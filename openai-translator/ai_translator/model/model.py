a559e9fa5f06dee78ee9d703e22e71ce07523cd4
mao chao <chaomao@gitlab.cn>
2024-03-22 16:08:37 +0800

    fix prompt

--- openai-translator/ai_translator/model/model.py
+++ openai-translator/ai_translator/model/model.py
@@ -4,11 +4,11 @@ from book import ContentType
 class Model:
     @staticmethod
     def make_text_prompt(text: str, target_language: str) -> str:
-        return f"翻译为{target_language}：{text}"
+        return f"翻译为{target_language}, 请保持原来的格式：\n{text}"
 
     @staticmethod
     def make_table_prompt(table: str, target_language: str) -> str:
-        return f"翻译为{target_language}，保持间距（空格，分隔符），以表格形式返回：\n{table}"
+        return f"将下面所有内容翻译为{target_language}，保持间距（空格，分隔符），以表格形式返回, 注意不要以 markdown 形式返回，表格头部和表格内容都翻译成中文：\n{table}"
 
     def translate_prompt(self, content, target_language: str) -> str:
         if content.content_type == ContentType.TEXT:
